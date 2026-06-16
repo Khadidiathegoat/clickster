bd9e02213b57b739dd4af9258da7a3903505b033
Khadidiatou <Kdjibril@brooklynsteamcenter.org>
2026-06-16 14:48:47 -0400

    added touches

--- src/main.js
+++ src/main.js
@@ -3,17 +3,17 @@ import { Howl } from 'howler'
 import soundEffect1 from './assets/ring.mp3'
 import soundEffect2 from './assets/wow.mp3'
 
-const clicker_noise = new Howl ({
+const clicker_noise = new Howl({
   src: [soundEffect1],
   autoplay: true,
-  loop:false, 
+  loop: false,
   volume: 0.5
 })
 
-const upgrade_now = new Howl ({
+const upgrade_now = new Howl({
   src: [soundEffect2],
   autoplay: true,
-  loop:false,
+  loop: false,
   volume: 1
 })
 
@@ -26,20 +26,20 @@ let clicker = document.getElementById("clicker")
 let scoreDisplay = document.getElementById("scoreDisplay")
 
 const formatter = new Intl.NumberFormat('en-US', {
-maximumFractionDigits: 2,
-notation: 'compact' ,
-compactDisplay: 'short'
+  maximumFractionDigits: 2,
+  notation: 'compact',
+  compactDisplay: 'short'
 })
 
-function updateScore (amount) {
+function updateScore(amount) {
   score += amount
-  scoreDisplay.innerText = score.toFixed(0) + ' cupcake points' 
+  scoreDisplay.innerText = score.toFixed(0) + ' cupcake points'
 
 }
-clicker.addEventListener("click" , function () {
+clicker.addEventListener("click", function () {
   let clickAnt = 1 + (cupcakes ** 1.05)
 
- clicker_noise.play()
+  clicker_noise.play()
 
   updateScore(clickAnt)
 })
@@ -50,13 +50,13 @@ let cupcakes = 0
 
 upgradeOne.addEventListener("click", function () {
   upgrade_now.play()
-  
-  
 
-  if (score>=100){
+
+
+  if (score >= 100) {
     updateScore(-100)
-    cupcakes++ 
-  if (cupcakes>=2){
+    cupcakes++
+    if (cupcakes >= 2) {
       upgradeOneCount.innerText = cupcakes + " Rare rainbow cupcakes<3"
     }
     else {
@@ -64,15 +64,38 @@ upgradeOne.addEventListener("click", function () {
     }
     upgradeOneCount.innerText = cupcakes + " Rare rainbow cupcake<3"
   } else {
-    alert ("YOU BROKE")
-
   }
+
 })
 
+
+let upgradeTwo = document.getElementById("upgradeTwo")
+let upgradeTwoCount = document.getElementById("upgradeTwoCount")
+
+upgradeTwo.addEventListener("click", function () {
+  upgrade_now.play()
+
+
+
+  if (score >= 500) {
+    updateScore(-500)
+    cupcakes++
+    if (cupcakes >= 2) {
+      upgradeTwoCount.innerText = cupcakes + " Ultra rare rainbow cupcakes<3"
+    }
+    else {
+      upgradeTwoCount.innerText = cupcakes + " Ultra rare rainbow cupcake<3"
+    }
+    upgradeTwoCount.innerText = cupcakes + " Ultra rare rainbow cupcake<3"
+  }
+
+})
+
+
 function gameLoop() {
-console.log ("Hi from gameLoop")
-let clickAnt = (cupcakes ** 1.05)
-updateScore(clickAnt)
+  console.log("Hi from gameLoop")
+  let clickAnt = (cupcakes ** 1.05)
+  updateScore(clickAnt)
 }
 
-setInterval (gameLoop, 1000)
+setInterval(gameLoop, 1000)
